a4454ee356cd724986cf750c5fe42fa346f6f8f6
anonfunc <anonfunc@gmail.com>
2016-02-26 12:48:33 -0800

    Homebrew goes at beginning of PATH

--- home/.sh.d/interactive/paths.sh
+++ home/.sh.d/interactive/paths.sh
@@ -9,6 +9,7 @@ my_add_path $HOME/Library/Python/2.7/bin
 my_add_path $HOME/.local/bin
 my_add_path $HOME/bin
 my_add_path /Applications/Emacs.app/Contents/MacOS/bin
+my_add_path /usr/local/bin
 
 if [[ -f /Applications/Emacs.app/Contents/MacOS/Emacs ]]
 then
